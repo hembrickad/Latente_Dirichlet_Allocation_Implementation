e57446e8b75cc26b750f4e1bd5b7d7eb320ed624
Adrienne Hembrick <43283730+hembrickad@users.noreply.github.com>
2022-11-25 13:53:17 -0500

    method set up

---
+++ Single_Thread_LDA.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <float.h>
+#include <math.h>
+
+private int k;
+private int *totalT;
+
+struct wordTopics{
+    char word[];
+    int *topics[k]
+}
+
+struct wordDocuments{
+    int document;
+    int popTopic;
+}
+
+
+int normalize(){
+    int num = 0;
+    int rand = 
+    float *norm = (float*) calloc(k, sizeOf(float));
+
+
+
+    free(norm);
+}
+
+float randNum(){
+    return
+}
+
+void topicDistribution(){
+
+
+}
+
+void wordDistribution(){
+
+
+
+}
+
+void documentToTopic(){
+
+
+}
+
+
+void wordToTopic(){
+
+
+}
+
+
+
+
+
+
+
+
+int main(int argc, char *argv[]){
+
+
+
+}
