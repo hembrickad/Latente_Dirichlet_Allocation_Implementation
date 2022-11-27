072b8ef59ce550b922944afe812fe89232f0f9ca
Adrienne Hembrick <43283730+hembrickad@users.noreply.github.com>
2022-11-27 08:30:05 -0500

    Moved methods to new file
    Moved a lot of set up methods and variables to a new file for the sake of clarity

--- SingleLDA.cpp
+++ SingleLDA.cpp
@@ -5,19 +5,44 @@
 #include <vector>
 #include <time.h>
 #include "Utils.cpp"
+#include "Preprocessing.cpp"
 
 using namespace std;
 
 const int k = 6;
-int total = 67;
+int totalWords = 0;
+vector<vector<int>> dTopics;
+vector<vector<int>> wTopics;
+vector<wordTopicsMatrix> wordTopicCount;
+
+struct wordTopicsMatrix{
+    string word;
+    int* topics;
+};
+
+struct docuTopicsMatrix{
+    string document;
+    int* topics;
+};
+
+struct wordTopics{
+    string word;
+    int t;
+};
+
+struct docuTopics{
+    string document;
+    int t;
+};
 
 
+//Normalizes current topic vector into a distribution
 vector<double> normalize(vector<double> topics){
     vector<double> topicsNorm;
     double n = 0;
 
     for(int i = 0; i < topics.size(); i++){
-        topicsNorm.push_back((double)topics.at(i)/(double)total);
+        topicsNorm.push_back((double)topics.at(i)/(double)totalWords);
     }
 
     for(int i = 0; i < topics.size(); i++){
@@ -32,6 +57,7 @@ vector<double> normalize(vector<double> topics){
     return topicsNorm;  
 }
 
+//Selects Random Topic Based on Given Distribution
 int randomTopicSelection(vector<double> distribution){
     srand(time(NULL));
     int i = 0;
@@ -46,11 +72,61 @@ int randomTopicSelection(vector<double> distribution){
     return i;
 }
 
+wordTopicsMatrix createWordTopic(string str){
+    int* ptr = (int*) calloc(k, sizeof(int));
+    wordTopicsMatrix word;
+    word.word = str;
+    word.topics = ptr;    
+    return word;
+}
+
+wordTopicsMatrix createWordTopic(string str){
+    int* ptr = (int*) calloc(k, sizeof(int));
+    wordTopicsMatrix word;
+    word.word = str;
+    word.topics = ptr;    
+    return word;
+}
+
+void cleanUp(){
+    for(wordTopicsMatrix n : wordTopicCount){
+        free(n.topics);
+    }
+
+    for(wordTopicsMatrix n : wordTopicCount){
+        free(n.topics);
+    }
+}
+
+
+//Extracts Unique Words
+vector<wordTopicsMatrix> setupTopicMatrix(vector<vector<string>> wordsInAbstracts){
+    vector<wordTopicsMatrix> words;
+    int i = -1;
+
+    for(vector<string> k: wordsInAbstracts){
+        for(string n : k){
+            for(wordTopicsMatrix x : words){
+                i = strcmp(n.c_str(), x.word.c_str());
+                if(i == 0)
+                    break;
+            }
+            if(i != 0)
+                words.push_back(createWordTopic(n));
+        }
+    }
+    return words;
+}
+
+
+
+
+
 
 
 int main(int argc, char **argv ){
 
-    randomTopicSelection(normalize());
+    //randomTopicSelection(normalize());
 
     vector<string> fp = Read_File("data/practice.csv");
     vector<vector<string>> titlesAndAbstracts;
@@ -67,15 +143,23 @@ int main(int argc, char **argv ){
         wordsInAbstracts.push_back(Split_String_To_Words(titleAndAbstractVtr));
     }
 
-    // For Each Document, Print Words In Document
-    for (vector<string> abstractWords : wordsInAbstracts)
-    {
-        for (string word : abstractWords)
-        {
-            printf("%s, ",word.c_str());
-        }
-        printf("\n");
+    vector<wordTopicsMatrix> words = setupTopicMatrix(wordsInAbstracts);
+
+    for(wordTopicsMatrix n: words){
+        printf("%s, \n",n.word.c_str());
     }
+
+
+
+    // For Each Document, Print Words In Document
+    // for (vector<string> abstractWords : wordsInAbstracts)
+    // {
+    //     for (string word : abstractWords)
+    //     {
+    //         printf("%s, ",word.c_str());
+    //     }
+    //     printf("\n");
+    // }
 
     printf("Complete");
 
